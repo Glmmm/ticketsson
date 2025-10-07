1d94a1260bef2228cb5514406a59313d61d2f35c
Guilherme Papa <64666460+Glmmm@users.noreply.github.com>
2025-10-06 22:35:12 -0300

    FEAT(major): adiciona cancelamento de reserva

--- client/src/app/routes/user/perfil/perfil.component.ts
+++ client/src/app/routes/user/perfil/perfil.component.ts
@@ -2,12 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ReservasService } from '../../../services/reservas.service';
 import { IReservas } from '../../../models/reservas.model';
 import { IUsuario } from '../../../models/usuario.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css',
-  imports: [],
+  imports: [DatePipe],
 })
 export class PerfilComponent implements OnInit {
   private service = inject(ReservasService);
@@ -23,5 +24,18 @@ export class PerfilComponent implements OnInit {
     this.service.buscarReservasUsuario(email).subscribe((response) => {
       this.reservas = response;
     });
+  }
+
+  cancelarReserva(reserva: IReservas) {
+    if (confirm(`Deseja cancelar a reserva "${reserva.ingresso.descricao}"`)) {
+      this.service.excluirReserva(reserva.id).subscribe(
+        (response) => {
+          this.listarReservas();
+        },
+        (error) => {
+          alert(error.error.message);
+        }
+      );
+    }
   }
 }
